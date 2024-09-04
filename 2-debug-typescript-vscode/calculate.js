e01cb9d32b014fcd55fd6a160b3fea5c08f6e92d
devoteeprashant <devoteeprashant@gmail.com>
2024-09-04 11:37:19 +0530

    3 type tò run

---
+++ 2-debug-typescript-vscode/calculate.js
@@ -0,0 +1,9 @@
+function sum(nums) {
+    var total = 0;
+    nums.forEach(function (num) {
+        total += num;
+    });
+    return total;
+}
+var numbers = [1, 2, 3, 4];
+console.log("The sum of ".concat(numbers, " is: "), sum(numbers));
